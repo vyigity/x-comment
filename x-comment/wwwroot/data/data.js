b091f972f618c569460873581acce71b17f867e5
vyigity <vyigity@hotmail.com>
2020-12-31 22:11:10 +0300

    Kurulum

--- x-comment/wwwroot/data/data.js
+++ x-comment/wwwroot/data/data.js
@@ -12,7 +12,15 @@ var comments = [
         parent: null,
         deletable: true,
         content: "2. yorum",
-        created: "2020-12-31T20:52:00Z",
+        created: "2020-12-31T21:52:00Z",
+        fullName: "Veli Yigit Yolcu"
+    },
+    {
+        id: 3,
+        parent: 2,
+        deletable: true,
+        content: "2. yoruma cevap",
+        created: "2020-12-31T22:52:00Z",
         fullName: "Veli Yigit Yolcu"
     }
 ];
